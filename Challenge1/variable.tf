2897c6fb345b3efc89ae65bd3d53775578ad2f4a
BivekGupta <123836085+BivekGupta@users.noreply.github.com>
2023-01-29 01:20:32 +0530

    Update variable.tf

--- Challenge1/variable.tf
+++ Challenge1/variable.tf
@@ -1,1 +1,7 @@
+#The format for variables used in the main.tf will be something like as mentioned below
 
+variable "bastion_ami"{
+  type = #This can be string/bool/integer
+  default = #This can be any default value or can be left empty
+}
+  
